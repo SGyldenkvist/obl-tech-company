5d5f867fdfd85fb62b53d7f398695e811672fdda
SGyldenkvist <silke@gyldenkvist.dk>
2025-10-01 14:30:11 +0200

    Single table assignments, aggregate functions, JOIN assignments

--- solution.sql
+++ solution.sql
@@ -93,7 +93,9 @@ SELECT SUM(salary + IFNULL(commission, 0)) AS total_salary_and_commission FROM e
 -- JOIN ASSIGMENTS:
 
 /* Opgave 1: Create an INNER JOIN between employees and departments to get the department name for each employee.
- Show all columns. (De har department_number til fælles).*/
+ Show all columns.
+
+   (De har department_number til fælles).*/
 
 SELECT * FROM employees
     INNER JOIN departments
@@ -105,4 +107,92 @@ SELECT * FROM employees
    Oh, and can you sort them alphabetically (A-Z)?
  */
 
- SELECT employees.employee_name, departments.department_name FROM employees JOIN departments ON employees.department_number = departments.department_number ORDER BY employees.employee_name ASC;
+ SELECT employees.employee_name, departments.department_name
+ FROM employees JOIN departments ON employees.department_number = departments.department_number
+ ORDER BY employees.employee_name ASC;
+
+
+/* Opgave 3: Now is the time to make a LEFT JOIN.
+   Let's look at employee_name and department_name only.
+   There is one more person this time who didn't show in the previous query.
+   Who is it and why?
+
+   (beholder left (FROM) table og tilføjer kun dem fra right (JOIN) ved dem de har til fælles)
+   Så den viser alle ansatte også dem uden et department number, som King, der er "president" og derfor uden direkte "tilknytning".
+
+
+ */
+
+
+SELECT employees.employee_name, departments.department_name
+FROM employees
+    LEFT JOIN departments
+    ON employees.department_number=departments.department_number;
+
+
+-- Opgave 4: Consider this query:
+
+SELECT departments.department_name, COUNT(employees.employee_number)
+FROM employees
+         JOIN departments
+              ON departments.department_number = employees.department_number
+GROUP BY department_name;
+
+-- One department is missing. Which one and why? (Look in the database).
+-- Det er department_name Operations med department_number 40 der mangler, fordi der ikke er nogle ansatte tilknyytet den afdeling.
+
+-- Opgave 5: To get the missing department change the previous query to use a RIGHT JOIN.
+SELECT departments.department_name, COUNT(employees.employee_number)
+FROM employees
+         RIGHT JOIN departments
+              ON employees.department_number = departments.department_number
+GROUP BY department_name;
+
+
+/*
+ Opgave 6: SCOTT sends you this query and asks you to run it.
+ In order to assess whether it is information that SCOTT is privy to, you must first understand it.
+ Describe in technical terms what this query does:
+
+ */
+
+SELECT *
+FROM employees employee
+         JOIN employees manager
+              ON employee.manager_id = manager.employee_number
+ORDER BY employee.employee_name;
+
+/*
+ "Vis alle kolonner fra tabellen employees, som vi kalder "employee"(alias),
+ sæt tabellen employees sammen med tabellen employees, som vi kalder manager(alias),
+ hvor mangager_id attributten i employees tabellen har samme værdi som employee_number i employees tabellen
+ Sorter alfabetisk efter employee_name attributten fra employees tabellen."
+ */
+
+ -- Opgave 7: Get two columns: employees and their managers.
+
+SELECT * FROM employees;
+
+SELECT employee.employee_name, manager.employee_name
+FROM employees employee
+         JOIN employees manager
+              ON employee.manager_id = manager.employee_number
+ORDER BY employee.employee_name;
+
+/* Opgave 8: Use the HAVING keyword (feel free to look it up) to show the departments with more than 3 employees.
+   The as number_of_employees is so that you can reference the value later on in the query:
+
+ */
+
+SELECT employees.department_number, COUNT(employees.department_number) AS number_of_employees
+FROM employees
+GROUP BY department_number
+HAVING COUNT(employees.department_number)>3;
+
+/*
+ Opgave 9: Subquery time!
+ Select the name and salary of employees whose salary is above average:
+ WHERE salary > (SELECT AVG(salary) FROM employees)
+ */
+
+ SELECT employee_name, salary FROM employees WHERE salary > (SELECT AVG(salary) FROM employees);
